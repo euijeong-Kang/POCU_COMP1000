a617c598bcb942dd5fc636035ae543e31496a47a
EuiJeong <wftw0121@naver.com>
2020-01-24 09:23:39 +0900

    modify

--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -8,19 +8,7 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
-            bool bOverflow = false;
-            BigNumberCalculator calc1 = new BigNumberCalculator(8, EMode.Decimal);
-
-            // AddOrNull test
-
-            
-            
-            
-
-            Debug.Assert(calc1.AddOrNull("-128", "0", out bOverflow) == "-128");
-            Debug.Assert(!bOverflow);
-            Debug.Assert(calc1.AddOrNull("0b1", "0b11111111", out bOverflow) == "-2");
-            Debug.Assert(!bOverflow);
+           
 
 
 
