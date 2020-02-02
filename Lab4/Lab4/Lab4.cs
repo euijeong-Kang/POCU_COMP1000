ef594e1f25a6b8acb9cf678b7905890b22d0f1a5
EuiJeong <wftw0121@naver.com>
2020-02-03 04:02:36 +0900

    c

--- Lab4/Lab4/Lab4.cs
+++ Lab4/Lab4/Lab4.cs
@@ -4,208 +4,39 @@ namespace Lab4
 {
     public sealed class MultiSet
     {
-        public List<string> Result;
         public void Add(string element)
         {
-            if (Result == null)
-            {
-                List<string> result = new List<string>();
-                result.Insert(0, element);
-                Result = result;
-            }
-            else
-            {
-                List<string> result = Result;
-                result.Insert(0, element);
-                Result = result;
-                Result.Sort();
-            }
+
         }
 
         public bool Remove(string element)
         {
-            bool bContain = false;
-            if (Result.Contains(element))
-            {
-                Result.Remove(element);
-                bContain = true;
-            }
-
-            return bContain;
+            return false;
         }
 
         public uint GetMultiplicity(string element)
         {
-            uint count = 0;
-            for (int i = 0; i < Result.Count; i++)
-            {
-                if (Result[i] == element)
-                {
-                    count++;
-                }
-            }
-            return count;
+            return 0;
         }
 
         public List<string> ToList()
         {
-            List<string> result = new List<string>();
-            if (Result != null)
-            {
-                for (int i = 0; i < Result.Count; i++)
-                {
-                    result.Add(Result[i]);
-                }
-                result.Sort();
-            }
-            return result;
+            return null;
         }
 
         public MultiSet Union(MultiSet other)
         {
-            MultiSet union = new MultiSet();
-            MultiSet copyUnion = new MultiSet();
-
-            List<string> copyResult = new List<string>();
-            List<string> copyOther = new List<string>();
-            if (Result != null)
-            {
-                for (int i = 0; i < Result.Count; i++)
-                {
-                    copyResult.Add(Result[i]);
-                }
-            }
-            if (other.Result != null)
-            {
-                for (int i = 0; i < other.Result.Count; i++)
-                {
-                    copyOther.Add(other.Result[i]);
-                }
-            }
-            if (Result == null || other.Result == null)
-            {
-                return union;
-            }
-            else
-            {
-                for (int i = 0; i < copyOther.Count; i++)
-                {
-                    copyResult.Add(copyOther[i]);
-                }
-                
-                MultiSet intersect = Intersect(other);
-                for (int i = 0; i < copyResult.Count; i++)
-                {
-                    copyUnion.Add(copyResult[i]);
-                }
-                MultiSet substract = copyUnion.Subtract(intersect);
-                for (int i = 0; i < substract.Result.Count; i++)
-                {
-                    union.Add(substract.Result[i]);
-                }
-                return union;
-            }
+            return null;
         }
 
         public MultiSet Intersect(MultiSet other)
         {
-            MultiSet intersect = new MultiSet();
-            List<string> copyResult = new List<string>();
-            
-            if (Result == null || other.Result == null)
-            {
-                return intersect;
-            }
-            else
-            {
-                if (Result != null)
-                {
-                    for (int i = 0; i < Result.Count; i++)
-                    {
-                        copyResult.Add(Result[i]);
-                    }
-                }
-                List<string> copyOther = new List<string>();
-                for (int i = 0; i < other.Result.Count; i++)
-                {
-                    copyOther.Add(other.Result[i]);
-                }
-                for (int j = 0; j < copyResult.Count; j++)
-                {
-                    for (int k = 0; k < copyOther.Count; k++)
-                    {
-                        if (copyResult[j] == copyOther[k])
-                        {
-                            intersect.Add(copyResult[j]);
-                            copyResult.Remove(copyResult[j]);
-                            copyOther.Remove(copyOther[k]);
-                            break;
-                        }
-                    }
-                }
-                if (intersect.Result != null)
-                {
-                    intersect.Result.Sort();
-                    return intersect;
-                }
-                else
-                {
-                    return intersect;
-                }
-            }
-
+            return null;
         }
 
         public MultiSet Subtract(MultiSet other)
         {
-            MultiSet subtract = new MultiSet();
-            if (Result == null)
-            {
-                return subtract;
-            }
-            else if (other.Result == null)
-            {
-                for (int i = 0; i < Result.Count; i++)
-                {
-                    subtract.Add(Result[i]);
-                }
-                return subtract;
-            }
-            else
-            {
-                List<string> copyResult = new List<string>();
-                List<string> copyOther = new List<string>();
-                for (int i = 0; i < Result.Count; i++)
-                {
-                    copyResult.Add(Result[i]);
-                }
-                for (int i = 0; i < other.Result.Count; i++)
-                {
-                    copyOther.Add(other.Result[i]);
-                }
-                for (int i = 0; i < copyResult.Count; i++)
-                {
-                    for (int j = 0; j < copyOther.Count; j++)
-                    {
-                        if (copyResult[i] == copyOther[j])
-                        {
-                            copyResult.Remove(copyResult[i]);
-                            copyOther.Remove(copyOther[j]);
-                            break;
-                        }
-                    }
-                }
-                if (copyResult != null)
-                {
-                    for (int i = 0; i < copyResult.Count; i++)
-                    {
-                        subtract.Add(copyResult[i]);
-                    }
-
-                }
-                return subtract;
-            }
-
+            return null;
         }
 
         public List<MultiSet> FindPowerSet()
